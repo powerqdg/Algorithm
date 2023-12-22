e6662def87c4aae736b04960479abcdb155a78a2
dgshin <powerqdg@gmail.com>
2023-12-22 11:28:58 +0900

    10430

---
+++ src/step/step01/Problem10430.java
@@ -0,0 +1,18 @@
+package step.step01;
+
+import java.util.Scanner;
+
+public class Problem10430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		System.out.println((a + b) % c);
+		System.out.println(((a % c) + (b % c)) % c);
+		System.out.println((a * b) % c);
+		System.out.println(((a % c) * (b % c)) % c);
+	}
+}
