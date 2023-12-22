9d5c03c2a606861c9c7d185b7df8f80ff1a80e39
dgsin <powerqdg@gmail.com>
2023-12-22 22:11:36 +0900

    15552

---
+++ src/step/step03/Problem15552.java
@@ -0,0 +1,25 @@
+package step.step03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem15552 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		int cnt = Integer.parseInt(br.readLine());
+		for (int i = 0; i < cnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a, b;
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			bw.write((a + b) + "\n");
+		}
+		bw.close();
+	}
+}
