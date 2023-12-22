d4cdd5a3c6f6ee3d4df913f5613a9218300fa4fd
dgshin <powerqdg@gmail.com>
2023-12-22 12:59:03 +0900

    11382

---
+++ src/step/step01/Problem11382.java
@@ -0,0 +1,15 @@
+package step.step01;
+
+import java.util.Scanner;
+
+public class Problem11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a, b, c;
+		a = sc.nextLong();
+		b = sc.nextLong();
+		c = sc.nextLong();
+		
+		System.out.println(a + b + c);
+	}
+}
