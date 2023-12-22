b2cc0a42e69cfc2cff6015654c3e4fda2eb2bdc7
dgshin <powerqdg@gmail.com>
2023-12-22 12:40:55 +0900

    18108

--- src/step/step01/Problem18108.java
+++ src/step/step01/Problem18108.java
@@ -1,5 +1,11 @@
 package step.step01;
 
-public class Problem18108 {
+import java.util.Scanner;
 
+public class Problem18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		System.out.println(year - 544 + 1);
+	}
 }
