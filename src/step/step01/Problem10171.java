477fe3bd80e29df7446b8045b5bd97122e8c0040
dgshin <powerqdg@gmail.com>
2023-12-22 13:00:41 +0900

    10171

---
+++ src/step/step01/Problem10171.java
@@ -0,0 +1,10 @@
+package step.step01;
+
+public class Problem10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
