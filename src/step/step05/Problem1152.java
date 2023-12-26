3929dc0362ee76f8b6dbc6de1c08953bf0e37f19
dgsin <powerqdg@gmail.com>
2023-12-26 23:03:18 +0900

    1152

---
+++ src/step/step05/Problem1152.java
@@ -0,0 +1,29 @@
+package step.step05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem1152 {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			bw.write(Integer.toString(st.countTokens()));
+			bw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (br != null) br.close(); } catch (IOException e) {}
+			try { if (bw != null) bw.close(); } catch (IOException e) {}
+		}
+	}
+}
