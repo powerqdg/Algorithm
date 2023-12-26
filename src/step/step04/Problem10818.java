90b76221cb7a83229eb07fde7b7bc40194f19bcc
dgshin <powerqdg@gmail.com>
2023-12-26 13:55:22 +0900

    10818

---
+++ src/step/step04/Problem10818.java
@@ -0,0 +1,58 @@
+package step.step04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem10818 {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			StringTokenizer st;
+			String line = null;
+			int lineNum = 0;
+			int arrLen = 0;
+			int[] intArr = null;
+			while ((line = br.readLine()) != null) {
+				lineNum++;
+				st = new StringTokenizer(line);
+				
+				if (!st.hasMoreTokens()) {
+					break;
+				}
+				
+				if (lineNum == 1) {
+					arrLen = Integer.parseInt(st.nextToken());
+				} else if (lineNum == 2) {
+					intArr = new int[arrLen];
+					for (int i = 0; i < intArr.length; i++) {
+						intArr[i] = Integer.parseInt(st.nextToken());
+					}
+					
+					int min = Integer.MAX_VALUE;
+					int max = Integer.MIN_VALUE;
+					
+					for (int i : intArr) {
+						min = Math.min(min, i);
+						max = Math.max(max, i);
+					}
+					bw.write(Integer.toString(min) + " " + Integer.toString(max));
+					bw.flush();
+				} 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (br != null) br.close(); } catch (IOException e) {}
+			try { if (bw != null) bw.close(); } catch (IOException e) {}
+		}
+	}
+}
