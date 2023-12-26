7dd8c5a051ba0dd2919ddc86ed1aaa2ef30a8f2c
dgsin <powerqdg@gmail.com>
2023-12-26 21:58:28 +0900

    11720

---
+++ src/step/step05/Problem11720.java
@@ -0,0 +1,33 @@
+package step.step05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem11720 {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			int strLen = Integer.parseInt(br.readLine());
+			int sum = 0;
+			String numStr = br.readLine();
+			for (int i = 0; i < strLen; i++) {
+				sum += Integer.parseInt(String.valueOf(numStr.charAt(i))); 
+			}
+			bw.write(Integer.toString(sum));
+			bw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (br != null) br.close(); } catch (IOException e) {}
+			try { if (bw != null) bw.close(); } catch (IOException e) {}
+		}
+	}
+}
