5596fb5c53434a33d8745b024918eec55f0390a8
dgshin <powerqdg@gmail.com>
2023-12-26 10:48:33 +0900

    10951 10952

--- src/step/step03/Problem10951.java
+++ src/step/step03/Problem10951.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Problem10952 {
+public class Problem10951 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,11 +18,6 @@ public class Problem10952 {
 			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-			if (a == 0 && b == 0) {
-				break;
-			}
-			
 			
 			bw.write(a + b + "\n");
 			bw.flush();
