79247d4cc5e87ce3e9ec544059291489b5b13890
dgshin <powerqdg@gmail.com>
2023-12-27 14:11:00 +0900

    11718

---
+++ src/step/step05/Problem11718.java
@@ -0,0 +1,31 @@
+package step.step05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem11718 {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			String line = null;
+			while ((line = br.readLine()) != null && !"".equals(line)) {
+				bw.write(line);
+				bw.newLine();
+			}
+			bw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (br != null) br.close(); } catch (IOException e) {}
+			try { if (bw != null) bw.close(); } catch (IOException e) {}
+		}
+	}
+}
