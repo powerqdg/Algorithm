05499da87177abcb6684e2bbea1859bbd92686a2
dgsin <powerqdg@gmail.com>
2024-01-06 17:11:33 +0900

    2903

---
+++ src/step/step08/Problem2903.java
@@ -0,0 +1,35 @@
+package step.step08;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem2903 {
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			String line = br.readLine();
+			if (line != null) {
+				int n = Integer.parseInt(line);
+				int pointNum = 2;
+				for (int i = 0; i < n; i++) {
+					pointNum += Math.pow(2, i);
+				}
+				bw.write(Integer.toString(pointNum * pointNum));
+			}
+			bw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { if (br != null) br.close(); } catch (IOException e) {}
+			try { if (bw != null) bw.close(); } catch (IOException e) {}
+		}
+	}
+}
