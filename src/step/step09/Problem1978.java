11e885d084e3364cd9d153ad4bf97fd24e4b058e
dgsin <powerqdg@gmail.com>
2024-01-07 23:29:20 +0900

    1978

--- src/step/step09/Problem1978.java
+++ src/step/step09/Problem1978.java
@@ -1,57 +1,51 @@
 package step.step09;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Problem1978 {
 	private BufferedReader reader;
-	private BufferedWriter writer;
-	
+
 	private void initializeReaderWriter() {
 		reader = new BufferedReader(new InputStreamReader(System.in));
-		writer = new BufferedWriter(new OutputStreamWriter(System.out));
 	}
-	
+
+	private boolean isPrime(int num) {
+		if (num < 2) {
+			return false;
+		}
+		for (int i = 2; i * i <= num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	private void solve() {
-		List<Integer> list = new ArrayList<Integer>();
 		try {
-			String line = reader.readLine();
-			line = reader.readLine();
-			StringTokenizer st = new StringTokenizer(line);
+			int n = Integer.parseInt(reader.readLine());
+			StringTokenizer st = new StringTokenizer(reader.readLine());
+
 			int cnt = 0;
 			while (st.hasMoreTokens()) {
-				int n = Integer.parseInt(st.nextToken());
-				for (int i = 1; i <= n; i++) {
-					if (n % i == 0) {
-						list.add(i);
-					}
-				}
-				
-				if (list.size() == 2) {
+				int num = Integer.parseInt(st.nextToken());
+				if (isPrime(num)) {
 					cnt++;
 				}
-				list.clear();
 			}
-			writer.write(Integer.toString(cnt));
-			writer.flush();
+			System.out.println(cnt);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void closeReaderWriter() {
 		try {
 			if (reader != null) {
 				reader.close();
-			}
-			if (writer != null) {
-				writer.close();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
