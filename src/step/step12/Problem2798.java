a4c6d5b3e762a55c7903b162f3eff8f68515fe40
dgshin <powerqdg@gmail.com>
2024-01-10 13:29:54 +0900

    2798

---
+++ src/step/step12/Problem2798.java
@@ -0,0 +1,81 @@
+package step.step12;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem2798 {
+	private BufferedReader reader;
+	private BufferedWriter writer;
+
+	private void initializeReaderWriter() {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		writer = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+	
+	
+
+	private void solve() {
+		try {
+			StringTokenizer st = new StringTokenizer(reader.readLine());
+			int cardCount = Integer.parseInt(st.nextToken());
+			int goalNumber = Integer.parseInt(st.nextToken());
+			int[] numArr = new int[cardCount];
+			st = new StringTokenizer(reader.readLine());
+			for (int i = 0; i < cardCount; i++) {
+				numArr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			Arrays.sort(numArr);
+			
+			int sum = 0;
+			int max = Integer.MIN_VALUE;
+			for (int i = cardCount - 1; i > -1; i--) {
+				int num1 = numArr[i];
+				for (int j = i - 1; j > -1; j--) {
+					int num2 = numArr[j];
+					for (int k = 0; k < j; k++) {
+						int num3 = numArr[k];
+						sum = num1 + num2 + num3;
+						if (sum <= goalNumber) {
+							max = Math.max(max, sum);
+						}
+					}
+				}
+			}
+			writer.write(Integer.toString(max));
+			writer.flush();
+		} catch (IOException e) {
+			handleIOException(e);
+		}
+	}
+
+	private void handleIOException(IOException e) {
+		System.err.println("An error occurred while reading input: " + e.getMessage());
+		e.printStackTrace();
+	}
+
+	private void closeReaderWriter() {
+		try {
+			if (reader != null) {
+				reader.close();
+			}
+			if (writer != null) {
+				writer.close();
+			}
+		} catch (IOException e) {
+			handleIOException(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		Problem2798 solution = new Problem2798();
+		solution.initializeReaderWriter();
+		solution.solve();
+		solution.closeReaderWriter();
+	}
+}
