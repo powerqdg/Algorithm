9efbb85485be588c50d930cb6d01ba76d50e3543
dgshin <powerqdg@gmail.com>
2024-01-11 10:02:07 +0900

    1436

---
+++ src/step/step12/Problem1436.java
@@ -0,0 +1,63 @@
+package step.step12;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem1436 {
+	private BufferedReader reader;
+	private BufferedWriter writer;
+
+	private void initializeReaderWriter() {
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		writer = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+
+	private void solve() {
+		try {
+			StringTokenizer st = new StringTokenizer(reader.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int cnt = 0;
+			int i = 0;
+			while (cnt < n) {
+				i++;
+				String strI = Integer.toString(i);
+				if (strI.indexOf("666") > -1) {
+					cnt++;
+				}
+			}
+			writer.write(Integer.toString(i));
+			writer.flush();
+		} catch (IOException e) {
+			handleIOException(e);
+		}
+	}
+
+	private void handleIOException(IOException e) {
+		System.err.println("An error occurred while reading input: " + e.getMessage());
+		e.printStackTrace();
+	}
+
+	private void closeReaderWriter() {
+		try {
+			if (reader != null) {
+				reader.close();
+			}
+			if (writer != null) {
+				writer.close();
+			}
+		} catch (IOException e) {
+			handleIOException(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		Problem1436 solution = new Problem1436();
+		solution.initializeReaderWriter();
+		solution.solve();
+		solution.closeReaderWriter();
+	}
+}
